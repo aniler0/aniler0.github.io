4afe28458b22ccac4fee314b0f393398c35e59a3
aniler0 <anil.er@hotmail.com>
2021-08-19 16:21:26 +0300

    :chart_with_upwards_trend: added google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,28 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { GlobalStyle } from "../styles/globals";
 import { MenuToggleProvider } from "../context/MenuContext";
+import * as ga from "../lib/ga";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <GlobalStyle />
