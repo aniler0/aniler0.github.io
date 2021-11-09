1d88b6b3e64e8b8e03d72cad0c40cdb035254b42
aniler0 <anil.er@hotmail.com>
2021-11-09 17:08:55 +0200

    new projects are added

--- utils/projects.js
+++ utils/projects.js
@@ -1,46 +1,66 @@
 const projects = [
-    {
-        id: '1',
-        projectName: 'Google Clone',
-        projectDetails: 'This is a clone project. You can search what you want like real Google.',
-        githubLink: 'https://github.com/aniler0/google-clone',
-        liveProject: 'https://ggle-clone1-1614854908220.web.app',
-        projectImage: '/projectimages/googleclone.png'
-    },
-    {
-        id: '2',
-        projectName: 'Full Stack Todo App',
-        projectDetails: 'This is my first Full-stack project. At backend side I use TypeScript,ExpressJS,Mongodb and JWT. At frontend side I use React.',
-        githubLink: 'https://github.com/aniler0/todo',
-        liveProject: 'https://todo-aniler0.vercel.app',
-        projectImage: '/projectimages/todoapp.png'
-    },
-    
-    {
-        id: '3',
-        projectName: 'Cat App',
-        projectDetails: 'This website provides a lot of cat things. You can get random cat images or information of cat breeds.',
-        githubLink: 'https://github.com/aniler0/catapp',
-        liveProject: 'https://catapp-git-master-aniler0.vercel.app',
-        projectImage: '/projectimages/catapp.png'
-    },
-    {
-        id: '4',
-        projectName: 'Weather Forecast App',
-        projectDetails: 'You can get weather forecast what country you want',
-        githubLink: 'https://github.com/aniler0/weatherforecast',
-        liveProject: 'https://weatherforecast-aniler0.vercel.app/',
-        projectImage: '/projectimages/weatherapp.png'
-    },
-    {
-        id: '5',
-        projectName: 'Space Game',
-        projectDetails: 'In this project, I have tried my ReactJS and algorithm skills with making this simple game. ',
-        githubLink: 'https://github.com/aniler0/spacegame',
-        liveProject: 'https://spacegame-2fb04.firebaseapp.com/',
-        projectImage: '/projectimages/spacegame.png'
-    },
+  {
+    id: "1",
+    projectName: "Movie Finder",
+    projectDetails: "Swipe your favorite movie left or right",
+    githubLink: "https://github.com/aniler0/movie-finder",
+    liveProject: "https://movie-finder-rho.vercel.app",
+    projectImage: "/projectimages/moviefinder.png",
+  },
+  {
+    id: "2",
+    projectName: "Expense Tracker",
+    projectDetails:
+      "The app records your expenses and you can see your income and expense.",
+    githubLink: "https://github.com/aniler0/expense-tracker",
+    liveProject: "https://expense-tracker-aniler0.vercel.app",
+    projectImage: "/projectimages/expensetracker.png",
+  },
+  {
+    id: "3",
+    projectName: "Google Clone",
+    projectDetails:
+      "This is a clone project. You can search what you want like real Google.",
+    githubLink: "https://github.com/aniler0/google-clone",
+    liveProject: "https://ggle-clone1-1614854908220.web.app",
+    projectImage: "/projectimages/googleclone.png",
+  },
+  {
+    id: "4",
+    projectName: "Full Stack Todo App",
+    projectDetails:
+      "This is my first Full-stack project. At backend side I use TypeScript,ExpressJS,Mongodb and JWT. At frontend side I use React.",
+    githubLink: "https://github.com/aniler0/todo",
+    liveProject: "https://todo-aniler0.vercel.app",
+    projectImage: "/projectimages/todoapp.png",
+  },
 
-]
+  {
+    id: "5",
+    projectName: "Cat App",
+    projectDetails:
+      "This website provides a lot of cat things. You can get random cat images or information of cat breeds.",
+    githubLink: "https://github.com/aniler0/catapp",
+    liveProject: "https://catapp-git-master-aniler0.vercel.app",
+    projectImage: "/projectimages/catapp.png",
+  },
+  {
+    id: "6",
+    projectName: "Weather Forecast App",
+    projectDetails: "You can get weather forecast what country you want",
+    githubLink: "https://github.com/aniler0/weatherforecast",
+    liveProject: "https://weatherforecast-aniler0.vercel.app/",
+    projectImage: "/projectimages/weatherapp.png",
+  },
+  {
+    id: "7",
+    projectName: "Space Game",
+    projectDetails:
+      "In this project, I have tried my ReactJS and algorithm skills with making this simple game. ",
+    githubLink: "https://github.com/aniler0/spacegame",
+    liveProject: "https://spacegame-2fb04.firebaseapp.com/",
+    projectImage: "/projectimages/spacegame.png",
+  },
+];
 
 export default projects;
